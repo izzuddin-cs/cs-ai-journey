41cf6db303c8dc6a342f741ba34c7007d87e8106
izzuddin-cs <mizzuddin0114@gmail.com>
2026-03-08 21:27:01 +0800

    Time: 3 ms (52.77%) | Memory: 20.4 MB (65.64%) - LeetSync

--- DSA/1-two-sum/two-sum.py
+++ DSA/1-two-sum/two-sum.py
@@ -1,12 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i,j]
-                
+        hashmap={}
+        
+        for i , num in enumerate(nums):
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff],i]
+            hashmap[num]=i
         return[]
+            
+        
+
+        
 
         
         
